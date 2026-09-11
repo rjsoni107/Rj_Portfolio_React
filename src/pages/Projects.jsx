f73d88f73465866319891a25ba401a4754f3df2c
Kuldeep Soni <rjsoni107@gmail.com>
2026-09-11 12:52:41 +0530

    Adjust project layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -116,7 +116,7 @@ export default function Projects() {
                 </div>
 
                 {/* Projects Grid */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 items-stretch">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 sm:gap-8 items-stretch">
                     {projects.map((project, idx) => (
                         <div
                             key={project.id}
